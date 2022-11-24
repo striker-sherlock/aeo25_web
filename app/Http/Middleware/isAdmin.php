f56aa9707d9a3058ae1dedd34fe412a3a49559e5
Okky Pratama <100081121+okkyPratama@users.noreply.github.com>
2022-11-24 21:49:48 +0700

    refactor:change homepage

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -17,6 +17,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
+        // dd(Auth::guard('admin')->check());
         if(!Auth::guard('admin')->check()){
             return redirect()->route('admin.login');
         }
