28373566c09e4b3632e133136749bea54fc2d3b8
Okky Pratama <100081121+okkyPratama@users.noreply.github.com>
2022-11-27 13:52:51 +0700

    update branch

--- app/Http/Controllers/UserCompetitionPaymentController.php
+++ app/Http/Controllers/UserCompetitionPaymentController.php
@@ -120,11 +120,15 @@ class UserCompetitionPaymentController extends Controller
 
     
     public function edit(CompetitionPayment $competitionPayment){
-        $paidSlot= CompetitionSlot::where('payment_id',$competitionPayment->id)->get();
+        $paidSlot= CompetitionSlot::where('payment_id', $competitionPayment->id)->get();
+        
         return view('competition-payments.edit',[
             'competitionPayment' => $competitionPayment,
             'paidSlot' =>$paidSlot,
-            'paymentProviders' => PaymentProvider::all()
+            'paymentProviders' => PaymentProvider::all(),
+            'user' => Auth::user(),
+            'slotId' => $competitionPayment->id,
+
         ]);
     }
 
