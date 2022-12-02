f76824ffeb139ebfeff9d34645c90f5b4a5c45ef
StefanBondito <stefanbondito@gmail.com>
2022-12-02 07:40:20 +0700

    fix: isAdmin fix

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -17,8 +17,9 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
+        // dd(Auth::guard('admin')->check());
         if(!Auth::guard('admin')->check()){
-            return redirect()->route('admin.login');    
+            return redirect()->route('admin.login');
         }
         return $next($request);
     }
