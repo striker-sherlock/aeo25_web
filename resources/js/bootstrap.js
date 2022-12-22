5b7e50cf6af1e9d45e6e8fa86a87c59d47e8cf00
Okky Pratama <100081121+okkyPratama@users.noreply.github.com>
2022-12-22 17:26:09 +0700

    feat: add animation in home and about page

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,19 +1,19 @@
-import _ from 'lodash';
+import _ from "lodash";
 window._ = _;
 
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
 window.bootstrap = bootstrap;
 
-import $ from 'jquery'
+import $ from "jquery";
 window.$ = $;
 
-import DataTable from 'datatables.net';
+import DataTable from "datatables.net";
 window.DataTable = DataTable;
 
-// Setup Datatable 
+// Setup Datatable
 $(document).ready(function () {
-    $('#dataTables').DataTable();
-    $('.dataTables').DataTable();
+    $("#dataTables").DataTable();
+    $(".dataTables").DataTable();
 });
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -21,50 +21,50 @@ $(document).ready(function () {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-
-
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 $(document).ready(function () {
+    $(".InstitutionContactTable").DataTable();
+    $(".FollowUpTable").DataTable();
+    $("#DoneFollowUpTable").DataTable();
+    $("#OnProgressFollowUpTable").DataTable();
+    $("#MediaPartnerTable").DataTable();
+    $("#InventoryTable").DataTable();
+    $("#AccessControlTable").DataTable();
 
-    $('.InstitutionContactTable').DataTable();
-    $('.FollowUpTable').DataTable();
-    $('#DoneFollowUpTable').DataTable();
-    $('#OnProgressFollowUpTable').DataTable();
-    $('#MediaPartnerTable').DataTable();
-    $('#InventoryTable').DataTable();
-    $('#AccessControlTable').DataTable();
+    $("#competitionCarousel").carousel({
+        interval: 10000,
+    });
 
-    $('#competitionCarousel').carousel({
-        interval: 10000
-      })
-      
-      $('.carousel .carousel-item').each(function(){
-          var minPerSlide = 3;
-          var next = $(this).next();
-          if (!next.length) {
-          next = $(this).siblings(':first');
-          }
-          next.children(':first-child').clone().appendTo($(this));
-          
-          for (var i=0;i<minPerSlide;i++) {
-              next=next.next();
-              if (!next.length) {
-                  next = $(this).siblings(':first');
-                }
-              
-              next.children(':first-child').clone().appendTo($(this));
+    $(".carousel .carousel-item").each(function () {
+        var minPerSlide = 3;
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(":first");
+        }
+        next.children(":first-child").clone().appendTo($(this));
+
+        for (var i = 0; i < minPerSlide; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(":first");
             }
-      });
-      
+
+            next.children(":first-child").clone().appendTo($(this));
+        }
+    });
+
+
 
 });
 
 // Enable Popover
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+);
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
+    return new bootstrap.Popover(popoverTriggerEl);
+});
